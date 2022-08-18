2cf76378e87398ae16b51bd74724ffd1adb7c2ec
Samuel Larsson <samuel@familjenlarsson.eu>
2022-08-18 11:30:43 +0200

    Updated songs url to new songlist
    Updated the SONGBOOK_DEFAULT_URL link to new songlist repo (https://github.com/insektionen/songlist)

--- app/src/main/java/se/insektionen/songbook/services/SongbookConfig.java
+++ app/src/main/java/se/insektionen/songbook/services/SongbookConfig.java
@@ -4,7 +4,7 @@ package se.insektionen.songbook.services;
  * Contains static configuration settings for the app.
  */
 public final class SongbookConfig {
-	public static final String SONGBOOK_DEFAULT_URL = "https://raw.githubusercontent.com/wsv-accidis/in-sangbok/master/sangbok/main.xml";
+	public static final String SONGBOOK_DEFAULT_URL = "https://raw.githubusercontent.com/insektionen/songlist/master/dist/songs-no-ids.xml";
 
 	private SongbookConfig() {
 	}
